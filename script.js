8b8dc002d486169bdc38a7a872209b407711153b
Chris Chan <christakleungchan@gmail.com>
2023-07-29 17:04:13 -0400

    Add button to remove books from list

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [new Book('1985', 'George Orwell', 300, false),
+let myLibrary = [new Book('1984', 'George Orwell', 300, false),
                     new Book('Green Eggs and Ham', 'Dr. Seuss', 72, true)];
 
 function Book(title, author, numPages, complete) {
@@ -12,10 +12,6 @@ Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.complete ? 'complete' : 'not read yet'}`;
 }
 
-function addBookToLibrary() {
-
-}
-
 function displayBooks(bookList) {
 
     let htmlBookList = document.querySelector('.book-list');
@@ -24,24 +20,43 @@ function displayBooks(bookList) {
     // Reset list
     while (htmlBookList.lastChild) { htmlBookList.removeChild(htmlBookList.lastChild) };
 
+    let index = 0;
 
     bookList.forEach((book) => {
         let card = document.createElement('li');
         let title = document.createElement('div');
         let author = document.createElement('div');
         let pages = document.createElement('div');
-        // add element for read status
+        let status = document.createElement('div');
 
         card.classList.add('card');
 
+        // Add information to title div
         title.textContent = book.title;
-        title.classList.add('book-title')
+        title.classList.add('book-title');
+
+        // Add information to author, pages, and status divs
         author.innerHTML = `<span class='bold'>Author: </span>${book.author}`;
         pages.innerHTML =`<span class='bold'>Pages: </span>${book.numPages}`;
+        status.innerHTML =`<span class='bold'>Status: </span>${book.complete ? 'Complete':'In progress'}`;
 
+        // Nest created divs to parent card element
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
+        card.appendChild(status);
+
+
+        // TODO: Render Delete button
+        let deleteBook = document.createElement('button');
+        deleteBook.textContent = "Remove";
+        deleteBook.addEventListener('click', removeBook);
+        card.appendChild(deleteBook);
+
+
+        card.dataset.index = index;
+        index++;
+
 
         htmlBookList.appendChild(card);
     });
@@ -68,9 +83,10 @@ function hideAddForm(form) {
     form.reset();
 }
 
-function updateLibrary(form, library) {
+function addBook(form, library) {
+
+    // Get form inputs and convert to Array to utilize Array methods
     let temp = form.querySelectorAll('input, select');
-    console.log(temp);
 
     let inputs = Array.from(temp);
 
@@ -78,9 +94,18 @@ function updateLibrary(form, library) {
 
     let [title, author, pages, status] = values;
 
+    // Create a new Book object with form values
     library.push(new Book(title, author, pages, (status==="complete" ? true:false)));
     hideAddForm(form);
+
+    // Re-render to UI with the updated array
     displayBooks(library);
+}
+
+function removeBook(e) {
+    let targetIndex = e.target.parentElement.dataset.index;
+    myLibrary.splice(targetIndex, 1);
+    displayBooks(myLibrary);
 }
 
 
@@ -94,6 +119,6 @@ formCancel.addEventListener('click', () => { hideAddForm(form) });
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    updateLibrary(e.target, myLibrary);
+    addBook(e.target, myLibrary);
 })
 
