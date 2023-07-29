c69b5c00148d90c50668a5817ce0b6834f424e10
Chris Chan <christakleungchan@gmail.com>
2023-07-28 21:28:54 -0400

    Add ability to add new book

--- script.js
+++ script.js
@@ -19,6 +19,11 @@ function addBookToLibrary() {
 function displayBooks(bookList) {
 
     let htmlBookList = document.querySelector('.book-list');
+    
+
+    // Reset list
+    while (htmlBookList.lastChild) { htmlBookList.removeChild(htmlBookList.lastChild) };
+
 
     bookList.forEach((book) => {
         let card = document.createElement('li');
@@ -27,7 +32,7 @@ function displayBooks(bookList) {
         let pages = document.createElement('div');
         // add element for read status
 
-        card.classList.add('card')
+        card.classList.add('card');
 
         title.textContent = book.title;
         title.classList.add('book-title')
@@ -39,9 +44,56 @@ function displayBooks(bookList) {
         card.appendChild(pages);
 
         htmlBookList.appendChild(card);
+    });
 
-        console.log(book);
-    })
+    // Render "new book" button
+    let buttonContainer = document.createElement('li');
+    let newBookButton = document.createElement('button');
+    newBookButton.textContent = "+";
+    newBookButton.classList.add('new-book-btn', 'card');
+    newBookButton.addEventListener('click', showAddForm); 
+
+
+    buttonContainer.appendChild(newBookButton);
+    htmlBookList.appendChild(buttonContainer);
 }
 
+function showAddForm() {
+    let form = document.querySelector('form');
+    form.style.visibility = 'visible';
+}
+
+function hideAddForm(form) {
+    form.style.visibility = 'hidden';
+    form.reset();
+}
+
+function updateLibrary(form, library) {
+    let temp = form.querySelectorAll('input, select');
+    console.log(temp);
+
+    let inputs = Array.from(temp);
+
+    let values = inputs.map((input) => input.value);
+
+    let [title, author, pages, status] = values;
+
+    library.push(new Book(title, author, pages, (status==="complete" ? true:false)));
+    hideAddForm(form);
+    displayBooks(library);
+}
+
+
+// MAIN SCRIPT
 displayBooks(myLibrary);
+
+let form = document.querySelector('form');
+let [formCancel, formConfirm] = document.querySelectorAll('form button');
+
+formCancel.addEventListener('click', () => { hideAddForm(form) });
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    updateLibrary(e.target, myLibrary);
+})
+
